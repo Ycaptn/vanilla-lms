8596ea70ae587bb5fa5bb90d70d0b21932cb2c94
Basil <105466598+Ycaptn@users.noreply.github.com>
2023-12-09 03:14:02 +0100

    Update entrypoint.app.sh

--- web-server/entrypoint.app.sh
+++ web-server/entrypoint.app.sh
@@ -11,6 +11,6 @@ else
     php artisan migrate 
     php artisan key:generate
     php artisan db:seed
-    # chown -R www-data:www-data storage
+    chown -R www-data:www-data storage
     exec apache2-foreground
 fi
